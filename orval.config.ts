9f90565003e0f2ba0483bc53b19110b27750a944
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-03-08 13:56:24 +0900

    fix orval config

--- orval.config.ts
+++ orval.config.ts
@@ -4,6 +4,11 @@ const apiDir = "./apps/api";
 const webDir = "./apps/web";
 const baseUrl = "http://localhost:8787";
 const docsPath = `${apiDir}/docs/openapi.yml`;
+const appName = "template";
+
+// axiosインスタンスのパス
+const axiosPath = `${webDir}/src/lib/axios.ts`;
+const axiosInstance = "customInstance";
 
 export default defineConfig({
 	client: {
@@ -12,12 +17,32 @@ export default defineConfig({
 		},
 		output: {
 			mode: "tags-split",
-			client: "axios",
-			target: `${webDir}/src/gen/`,
-			schemas: `${webDir}/src/gen/models/`,
+			client: "react-query",
+			target: `${webDir}/src/generated/api`,
+			schemas: `${webDir}/src/generated/schemas`,
 			clean: true,
 			baseUrl,
-			mock: true,
+			override: {
+				mutator: {
+					path: axiosPath,
+					name: axiosInstance,
+				},
+				query: {
+					useSuspenseQuery: true,
+					useSuspenseInfiniteQuery: true,
+					useInfiniteQueryParam: "page",
+					usePrefetch: true,
+				},
+			},
+		},
+	},
+	zod: {
+		input: { target: docsPath },
+		output: {
+			mode: "tags-split",
+			client: "zod",
+			target: `${webDir}/src/generated/api`,
+			fileExtension: ".zod.ts",
 		},
 	},
 	api: {
@@ -25,14 +50,18 @@ export default defineConfig({
 			target: docsPath,
 		},
 		output: {
-			mode: "tags-split",
+			biome: true,
+			mode: "split",
 			client: "hono",
-			target: `${apiDir}/src/gen/`,
+			target: `${apiDir}/src/adapters/${appName}.ts`,
 			override: {
 				hono: {
 					handlers: `${apiDir}/src/adapters/handler/`,
 				},
 			},
+		},
+		hooks: {
+			afterAllFilesWrite: "pnpm format-and-lint:fix --unsafe",
 		},
 	},
 });
